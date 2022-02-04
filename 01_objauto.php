38db0d88ccc79a8b0fb7bceced89e4876d6310a4
JHONATANALEJANDROURREAESCOBAR <jhonatan.urrea@misena.edu.co>
2022-02-04 15:38:20 -0500

    repositorio

---
+++ 01_objauto.php
@@ -0,0 +1,18 @@
+<?php
+require_once("01_ejercicio.php");
+
+$objVehiculo = new Vehiculo("ZHZ-354","Mazda",2020,455200,70,30);
+$objVehiculo->getVehiculo();
+
+print_r('<pre>');
+print_r($objVehiculo);
+print_r('</pre>');
+
+echo"<br>";
+echo $objVehiculo->marca;
+echo"<br>";
+echo"La distancia recorrida es: " . $objVehiculo->calculo_distancia();
+
+
+
+?>
